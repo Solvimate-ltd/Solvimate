0fa9e1bad2dfd1dbdf05237371887866e0aa9d69
itzsourab <sv059813@gmail.com>
2024-11-10 21:55:59 +0530

    major changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,77 @@
-import "./FooterStyles.css"
-import React from 'react'
+import {
+  FaFacebook,
+  FaHome,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+  FaTwitter,
+} from "react-icons/fa";
+import "./FooterStyles.css";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
 
-        
+
+          <div className="location">
+
+
+            <FaHome size={20} style={{ color: "#fff", marginRight:"2rem" }} />
+              <p> 123 Housing society.</p>
+              <p>Bangladesh.</p></div>
+            
+
+         
+
+
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              1-2-3-4-5-6-7-8-9
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              solvimate email
+            </h4>
+          </div>
+        </div>
+
+        <div className="right">
+          <h4>About the company</h4>
+          <p>
+            Direction Educare, a trailblazer in the content industry, cultivates
+            knowledge through innovative solutions, fostering learning
+            excellence for a brighter future.
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
