90ec3075a291f5da9d8b15b2fe3bdcde0ae0ccf1
itzsourab <sv059813@gmail.com>
2024-11-29 15:54:35 +0530

    map location update

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -43,7 +43,7 @@ const ContactForm = () => {
             <iframe 
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3636592.3789535128!2d78.21790495666548!3d27.113476843040225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39994e9f7b4a09d3%3A0xf6a5476d3617249d!2sUttar%20Pradesh!5e0!3m2!1sen!2sin!4v1731492916089!5m2!1sen!2sin" 
             width="100%" 
-            height="200" 
+            height="400" 
             style={{ border: 0 }} 
             allowFullScreen 
             loading="lazy" 
